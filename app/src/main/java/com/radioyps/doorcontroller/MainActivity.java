c42106886e0e0f91a9e39a8478683a23db1262fb
peter.ye <peter.ye.datawind@gmail.com>
2016-09-25 21:28:35 -0400

    adding network connection exception code

--- app/src/main/java/com/radioyps/doorcontroller/MainActivity.java
+++ app/src/main/java/com/radioyps/doorcontroller/MainActivity.java
@@ -17,6 +17,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
@@ -26,17 +29,33 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static int connectPort = 5028;
     private static String OPEN_THE_DOOR = "A412..&35?@!";
-    private static String response = null;
+
     private final static int SOCKET_TIMEOUT = 10 * 000; /*10 seconds */
     private final static String IP_ADDR = "192.168.12.238";
     private final static String NETWORK_ERROR = "network error";
     private static boolean isCmdFinished = true;
     private static Context mContext = null;
 
+    private final static int RESULT_SUCCESS = 0x11;
+    private final static int RESULT_IO_ERROR = 0x12;
+    private final static int RESULT_TIMEOUT = 0x13;
+
+    private final static int RESULT_HOST_UNAVAILABLE = 0x14;
+    private final static int RESULT_HOST_REFUSED = 0x15;
+    private final static int RESULT_NETWORK_UNREACHABLE = 0x16;
+    private final static int RESULT_HOSTNAME_NOT_FOUND = 0x17;
+    private final static int RESULT_UNKNOWN = 0x18;
+
+    private final static String  EXCEPTION_NETWORK_UNREACHABLE = "ENETUNREACH";
+    private final static String  EXCEPTION_HOST_UNAVAILABLE = "EHOSTUNREACH";
+    private final static String  EXCEPTION_HOST_REFUSED = "ECONNREFUSED";
+    private static int response = RESULT_UNKNOWN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+		/*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mContext = this;
@@ -48,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+		*/
     }
 
     @Override
@@ -99,12 +119,85 @@ public class MainActivity extends AppCompatActivity {
 
         private final String LOG_TAG = sendCmdOverTcpTask.class.getSimpleName();
 
+
+		private String getStatusString(int status){
+			String ret = getString(R.string.result_cmd_unknown);
+			switch(status){
+                 case RESULT_HOST_REFUSED:
+                     ret = getString(R.string.result_cmd_host_refused);
+                     break;
+                case RESULT_HOST_UNAVAILABLE:
+                    ret = getString(R.string.result_cmd_host_unavailable);
+                    break;
+                case RESULT_HOSTNAME_NOT_FOUND:
+                    ret = getString(R.string.result_cmd_hostname_not_found);
+                    break;
+                case RESULT_IO_ERROR:
+                    ret = getString(R.string.result_cmd_io_error);
+                    break;
+                case RESULT_NETWORK_UNREACHABLE:
+                    ret = getString(R.string.result_cmd_network_unreachable);
+                    break;
+                case RESULT_TIMEOUT:
+                    ret = getString(R.string.result_cmd_timeout);
+                    break;
+                case RESULT_SUCCESS:
+                    ret = getString(R.string.result_cmd_success);
+                    break;
+                default:
+                    ret = getString(R.string.result_cmd_unknown);
+
+
+
+            }
+            return ret;
+
+		}
+
+		private int getConnectionErrorCode(String error){
+			int ret = RESULT_UNKNOWN;
+
+			if(error.indexOf(EXCEPTION_NETWORK_UNREACHABLE) != -1){
+                  ret =  RESULT_NETWORK_UNREACHABLE;
+			}else if(error.indexOf(EXCEPTION_HOST_UNAVAILABLE) != -1){
+                  ret =  RESULT_HOST_UNAVAILABLE;
+			}else if(error.indexOf(EXCEPTION_HOST_REFUSED) != -1){
+                  ret = RESULT_HOST_REFUSED;
+			}
+            return ret;
+
+		}
+		private String getConnectionError(ConnectException ex){
+
+
+			String ret = null;
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String exceptionStr = sw.toString(); 
+			String lines[] = exceptionStr.split("\\r?\\n");
+			for(int i = 0; i < lines.length; i++){
+				 if(lines[i].indexOf("ConnectException") != -1){
+					 ret = lines[i];
+					 break;
+				 }
+			 }
+
+
+            Log.d(TAG, "getConnectionError()>> Line: " + ret);
+			return ret;
+
+		}
+
+
         @Override
         protected void onPostExecute(String[] strings) {
             super.onPostExecute(strings);
+            String errorStr = getStatusString(response);
+            Log.d(TAG, "toggleDoor()>> status of cmd: " + errorStr);
             Toast.makeText(mContext,
-                    getString(R.string.success_on_cmd), Toast.LENGTH_LONG).show();
-            Log.d(TAG, "toggleDoor()>> response: " + response);
+                    errorStr, Toast.LENGTH_LONG).show();
         }
 
 
@@ -114,12 +207,12 @@ public class MainActivity extends AppCompatActivity {
 
 
             Socket socket = null;
-
+            String stringReceived = "";
 
 
             try {
 
-                response = "";
+                response = RESULT_UNKNOWN;
 
                 socket = new Socket(IP_ADDR, connectPort);
                 socket.setSoTimeout(SOCKET_TIMEOUT);
@@ -137,49 +230,43 @@ public class MainActivity extends AppCompatActivity {
                 outputStream.write(OPEN_THE_DOOR.getBytes());
                 outputStream.flush();
 
-//                Toast.makeText(mContext,
-//                        "Sending String ", Toast.LENGTH_LONG).show();
-			/*
-			 * notice: inputStream.read() will block if no data return
-			 */
 
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     byteArrayOutputStream.write(buffer, 0, bytesRead);
-                    response += byteArrayOutputStream.toString("UTF-8");
+                    stringReceived += byteArrayOutputStream.toString("UTF-8");
                 }
                 outputStream.close();
                 inputStream.close();
+                response = RESULT_SUCCESS;
+
+            } catch (ConnectException e) {
+                e.printStackTrace();
+		        String errorStr = getConnectionError(e);
+                if(errorStr != null)
+				response =  getConnectionErrorCode(errorStr);
+                else
+                    response = RESULT_UNKNOWN;
 
             } catch (UnknownHostException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
-                response = "UnknownHostException: " + e.toString();
+                response = RESULT_HOSTNAME_NOT_FOUND ;
 
-//                Toast.makeText(mContext,
-//                        "UnknownHostException: ", Toast.LENGTH_LONG).show();
             } catch (SocketTimeoutException e) {
 
-                Log.i(LOG_TAG, "sendCmdOverTcpTask()>> exception on TIMEOUT error ");
-//                Toast.makeText(mContext,
-//                        "TIMEOUT error ", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
-                response = NETWORK_ERROR;
+                response = RESULT_TIMEOUT;
+
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
-                //response = "IOException: " + e.toString();
-                response = NETWORK_ERROR;
-//                Toast.makeText(mContext,
-//                        "NETWORK ERROR", Toast.LENGTH_LONG).show();
+                response = RESULT_IO_ERROR;
+
+
             } finally {
+                isCmdFinished = true;
                 if (socket != null) {
                     try {
                         socket.close();
-                        isCmdFinished = true;
-//                        Toast.makeText(mContext,
-//                                "Close Socket", Toast.LENGTH_LONG).show();
                     } catch (IOException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                 }
---
+++ app/src/main/java/com/radioyps/doorcontroller/Utils.java
@@ -0,0 +1,10 @@
+package com.radioyps.doorcontroller;
+
+/**
+ * Created by yep on 25/09/16.
+ */
+public class Utils {
+
+
+
+}
