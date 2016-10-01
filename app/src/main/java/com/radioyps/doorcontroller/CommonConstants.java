9e3db70137b0836269a4ef6f5ba102d458c6229f
peisheng.ye <peisheng.ye.88@gmail.com>
2016-10-01 10:46:41 -0400

    added stop/start for finding controller

--- app/src/main/java/com/radioyps/doorcontroller/CommonConstants.java
+++ app/src/main/java/com/radioyps/doorcontroller/CommonConstants.java
@@ -11,6 +11,8 @@ public final class CommonConstants {
     }
     public static final String ACTION_PING = "com.radioyps.doorcontroller.ACTION_PING";
     public static final String ACTION_PRESS_DOOR_BUTTON = "com.radioyps.doorcontroller.ACTION_PRESS_DOOR_BUTTON";
+    public static final String ACTION_STOP_CONNECTING = "com.radioyps.doorcontroller.ACTION_STOP_CONNECTING";
+    public static final String ACTION_START_CONNECTING = "com.radioyps.doorcontroller.ACTION_START_CONNECTING";
     public static final int MSG_UPDATE_WIFI_STATUS = 0x10;
     public static final int MSG_UPDATE_BUTTON_STATUS = 0x11;
     public static final int MSG_UPDATE_CMD_STATUS = 0x12;
@@ -26,5 +28,6 @@ public final class CommonConstants {
     public static String ACK_PRESS_DOOR_BUTTON =  "B835??/!xx";
     public final static int SOCKET_TIMEOUT = 10 * 000; /*10 seconds */
     public final static String IP_ADDR = "192.168.12.238";
+    //public final static String IP_ADDR = "192.168.12.240";
     public final static String NETWORK_ERROR = "network error";
 }
